e59a0511500fdd12b6a216fced2c63b854bdff35
tangerineYR <dbfl040926@naver.com>
2025-06-01 02:14:04 +0900

    Create Main.py

---
+++ gmm_classifier_feature/Main.py
@@ -0,0 +1,10 @@
+from Source import CreateFeatureDataset
+from Source import EvaluateModel
+
+
+def main():
+    CreateFeatureDataset.main()
+    EvaluateModel.main()
+
+if __name__ == '__main__':
+    main()
